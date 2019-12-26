3184994fb0e9a0d04ddc5c97c48634714a816b28
sergeinemogai <nemogaiergei1998@gmail.com>
2019-12-26 12:10:36 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //TODO: implement the previous algorithms using a java.util.concurrent
+        System.out.println("Nothing to do yet");
+    }
+}
